ea97267f9d1b770f1333d504b4167f39289e308b
Alexander <acoundou@gmail.com>
2017-01-26 15:31:19 -0500

    Initial Commit

---
+++ ChatCLient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatCLient
+//
+//  Created by Alex Coundouriotis on 1/26/17.
+//  Copyright © 2017 ACApplications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
